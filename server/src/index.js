3774d26c043f2dbc6658c480ba0ccbe2710c7002
Estefan Hu <estefanhu074@gmail.com>
2020-06-02 14:38:29 -0700

    connected to local mongodb instance

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+const DB_CONNECTION = process.env.APP_DB || 'location_tracker';
+
+mongoose.connect(`mongodb://localhost/${DB_CONNECTION}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+mongoose.connection.once('open', () => {
+  console.log(`connection has been established to ${DB_CONNECTION}`);
+}).on('err', err => {
+  console.error('Connection Error: ' + err);
+});
 
 app.get('/', (req,res) => {
 	res.send('hello world');
