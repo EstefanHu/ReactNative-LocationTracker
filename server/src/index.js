dff99330b1c9bb09aaf3120ea8f48915a822942a
Estefan Hu <estefanhu074@gmail.com>
2020-06-02 14:51:18 -0700

    prepping for models

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
+const authRoutes = require('./routes/authRoutes');
+
+app.use(bodyParser.json());
 
 const DB_CONNECTION = process.env.APP_DB || 'location_tracker';
 
 mongoose.connect(`mongodb://localhost/${DB_CONNECTION}`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  });
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	});
 mongoose.connection.once('open', () => {
-  console.log(`connection has been established to ${DB_CONNECTION}`);
+	console.log(`connection has been established to ${DB_CONNECTION}`);
 }).on('err', err => {
-  console.error('Connection Error: ' + err);
+	console.error('Connection Error: ' + err);
 });
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
 	res.send('hello world');
 });
 
-app.listen(3000, () => {
-console.log('Listening on port 3000');
+app.use(authRoutes);
+
+app.listen(4000, () => {
+	console.log('Listening on port 4000');
 });
---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  try {
+    await checkUser = 
+  } catch(error) {
+    
+  }
+});
+
+module.exports = router;
