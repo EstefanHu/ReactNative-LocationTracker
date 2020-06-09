fdecc3282d6e0a4ee5b2a38ab7291c2a5901d2f4
Estefan Hu <estefanhu074@gmail.com>
2020-06-09 13:46:05 -0700

    added state managment of mapview

--- mobile/src/components/map.js
+++ mobile/src/components/map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   StyleSheet
@@ -6,9 +6,24 @@ import {
 import MapView from 'react-native-maps';
 
 export const Map = () => {
-  return <MapView />
+  const [region, setRegion] = useState({
+    latitude: 37.33233,
+    longitude: -122.03121,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const onRegionChange = region => setRegion(region);
+
+  return <MapView
+    style={styles.map}
+    initialRegion={region}
+    onRegionChange={onRegionChange}
+  />
 }
 
 const styles = StyleSheet.create({
-
+  map: {
+    height: 300
+  }
 });
