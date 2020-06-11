1488455128595ba146865eec21acb9f29a3e8eac
Estefan Hu <estefanhu074@gmail.com>
2020-06-10 19:39:38 -0700

    added location logic

--- mobile/src/components/trackForm.js
+++ mobile/src/components/trackForm.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {
   StyleSheet,
   TextInput,
-  Button
+  Button,
+  View
 } from 'react-native';
 
 export const TrackForm = () => {
   return (
-    <>
+    <View style={styles.container}>
       <TextInput
         style={styles.input}
         placeholder='Enter Name'
@@ -16,16 +17,24 @@ export const TrackForm = () => {
         style={styles.button}
         title='Start Recording'
       />
-    </>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    paddingVertical: 5,
+  },
   input: {
     height: 10,
     width: '80%',
+    paddingVertical: 15,
+    borderBottomColor: 'grey',
+    borderBottomWidth: 1,
   },
   button: {
-    
+    backgroundColor: 'blue',
+    width: '80%',
   }
 });
